3596e68bfc3a237c15c1899c0578c9e345a7cec2
Horace Zheng <horace.zheng@gmail.com>
2013-09-07 00:32:23 +0800

    first time load index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var os = require("os");
+var url = require("url");
+
+http.createServer(res).listen(80);
+console.log("Server running at " + os.hostname());
+
+function res(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("what's up?");
+  response.end();
+
+  var time = new Date().toLocaleString().substr(0, 33);
+  console.log("[Request] - [" + time + "]: request for " + request.url);
+}
