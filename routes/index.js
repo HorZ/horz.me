55e4396720d95db558076e7560deb07110eb14ac
HorZ <m@horz.me>
2013-09-11 01:03:16 -0700

    add express.js component

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  var info = {};
+
+  info['sessionID'] = req.sessionID;
+  // info['req.params'] = req.params;
+  // info['req.query'] = req.query;
+  // info['req.route'] = req.route;
+  // info['req.cookies'] = req.cookies;
+  // info['req.get()'] = req.get();
+  // info['req.accepts()'] = req.accepts();
+  // info['req.accepted'] = req.accepted;
+  // info['req.is()'] = req.is();
+  // info['req.ip'] = req.ip;
+  // info['req.ips'] = req.ips;
+  // info['req.path'] = req.path;
+  // info['req.host'] = req.host;
+  info['req.fresh'] = req.fresh;
+  info['req.stale'] = req.stale;
+  info['req.xhr'] = req.xhr;
+  info['req.protocal'] = req.protocal;
+  info['req.subdomains'] = req.subdomains;
+  info['req.originalUrl'] = req.originalUrl;
+
+  var values = {
+  	title: 'horz.me', 
+  	zone: 'test zone', 
+  	anyinfo: JsonToString(info)
+  }
+
+  res.render('index', values);
+
+};
+
+function JsonToString(o) {    
+    var arr = []; 
+    var fmt = function(s) { 
+        if (typeof s == 'object' && s != null) return JsonToString(s); 
+        return /^(string|number)$/.test(typeof s) ? "\"" + s + "\"" : s; 
+    } 
+    for (var i in o) 
+         arr.push("\"" + i + "\":" + fmt(o[i])); 
+    return "{" + arr.join(",") + "}"; 
+} 
---
+++ simpletest.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var os = require("os");
+var url = require("url");
+var fs = require("fs");
+
+http.createServer(res).listen(80);
+console.log("Server running at " + os.hostname());
+
+function res(request, response) {
+    route(request, response);
+
+    var time = new Date().toLocaleString().substr(0, 33);
+    console.log("[Request] - [" + time + "]: request for " + request.url);
+}
+
+function route(request, response) {
+    var path = url.parse(request.url).pathname;
+
+    if (path == "/favicon.ico") {
+        var icon = fs.readFileSync("favicon.ico");
+        response.writeHead(200, {"Content-Type": "image"});
+        response.write(icon);
+    } else {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("what's up, there?");
+    }
+    response.end();
+}
